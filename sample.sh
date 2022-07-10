4e0a1eb074ac8e81a25d46b15a8226161695b488
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-10 18:24:46 +0900

    ADD:add comment, shell sripts

---
+++ sample.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#: Title	: Sample bash script
+#: Date 	: 2022-07-10
+#: Author	: Hyeonjun Suh ( guswns0208@naver.com )
+#: Version	: 1.0
+#: Descriptionn	: Hello world test
+echo "Today : $(date +%Y-%m-%d)"
+echo "Hello, World!"
