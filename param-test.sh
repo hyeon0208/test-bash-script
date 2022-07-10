2c5a5ef6bc37714f0f5e98c8b3de56a9d5076977
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-10 19:14:18 +0900

    ADD:parameter-test sh

---
+++ param-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#: Usage	: param-test.sh arg1 arg2 arg3
+echo "The Script name: $0"
+echo "The first argument: $1"
+echo "The second argument: $2"
+echo "The numner of arguments: $#"
+echo "The list of arguments: $@"
+echo "The list of arguments: $*"
