d5b5b93618baa8c16740b8ad0f73e04f813b7cd2
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-10 18:32:43 +0900

    Fix: Directory, varUsage check scripts

--- varUsage.sh
+++ varUsage.sh
@@ -4,7 +4,7 @@
 #: Author       : Hyeonjun Suh ( guswns0208@naver.com )
 #: Version      : 1.0
 #: Descriptionn : du 명령으로 변동량을 확인 할 수 있다. (error 코드는 출력안함)
-echo " [ /var Directiory ]"
+echo " [ ~/test-bash-script Directiory ]"
 echo "---------------------"
 echo "Today is $(date +%m-%d)"
 echo "---------------------"
