3bb7507c953070dc4b9cf3570f51e3aa4f3380a7
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-11 19:03:11 +0900

    ADD:Auto dirfile save to txt shell

---
+++ dir-filesave-to-text.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#: Usage	: 입력한 디렉토리의 모든 파일을 홈 디렉토리에 날짜.txt 파일로 저장합니다.
+#: Author	: Hyeon jun Suh 
+echo -n "Input Diretory name :"; read dirname
+ls $dirname > ~/$(date +%Y-%m-%d).txt
+cat ~/$(date +%Y-%m-%d).txt
+check=`cat -n ~/$(date +%Y-%m-%d).txt | wc -l`
+echo $check
+echo -n "check disk Usage (y/n)? "; read answer
+if [ $answer == y ]; then
+        du -sh $dirname 2> /dev/null
+        echo
+else if [ $answer == n ]; then
+        echo "nothing to show"
+else
+        echo "wrong input"
+fi
