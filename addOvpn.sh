1b5a775b55baf67ad1e7da73575010828aa11476
H <99153215+SuhHyeonjun@users.noreply.github.com>
2022-09-08 09:39:15 +0900

    ovpn 파일 생성 sh

---
+++ addOvpn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Usage         : Client 인증서를 가지고 .ovpn 파일 생성.
+
+KEY_DIR=~/client-configs/keys
+OUTPUT_DIR=~/client-configs/files
+BASE_CONFIG=~/client-configs/base.conf
+
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-auth>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-auth>') \
+    > ${OUTPUT_DIR}/${1}.ovpn
